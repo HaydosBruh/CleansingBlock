a4c6e40f11ecf5cbcd5638529c58b166ebe58120
HaydosBruh <62004533+HaydosBruh@users.noreply.github.com>
2021-06-27 15:28:23 +1000

    Add project files.

---
+++ CleansingBlock/CleansingBlock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BepInEx;
+
+namespace CleansingBlock
+{
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInPlugin("org.bepinex.plugins.CleansingBlock", "Cleansing Block Plug-In", "1.0.0.0")]
+    [BepInProcess("Rounds.exe")]
+    public class CleansingBlock : BaseUnityPlugin
+    {
+        void Awake()
+        {
+            UnityEngine.Debug.Log("Hello, world!");
+        }
+    }
+}
